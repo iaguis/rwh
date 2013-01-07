a94afce78a7d66d926c9264e6da4107b7e444a51
Iago López Galeiras <iaguis@gmail.com>
2013-01-07 18:07:02 +0100

    ch04: Add firstWords

---
+++ ch04/InteractWith.hs
@@ -0,0 +1,22 @@
+-- file: ch04/InteractWith.hs
+-- Save this in a source file, e.g. Interact.hs
+
+import System.Environment (getArgs)
+import Data.Char
+
+interactWith function inputFile outputFile = do
+  input <- readFile inputFile
+  writeFile outputFile (function input)
+
+main = mainWith myFunction
+  where mainWith function = do
+          args <- getArgs
+          case args of
+            [input,output] -> interactWith function input output
+            _ -> putStrLn "error: exactly two arguments needed"
+
+        -- replace "id" with the name of our function below
+        myFunction = firstWord
+
+firstWord :: String -> String
+firstWord s = unlines $ map (head . words) (lines s)
