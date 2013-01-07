3a5d01de85c7b1681bb45889107f0d919a057253
Iago López Galeiras <iaguis@gmail.com>
2013-01-07 17:09:52 +0100

    Finished convex hull implementation

--- ch03/convex_hull.hs
+++ ch03/convex_hull.hs
@@ -1,8 +1,13 @@
 import Data.List
+import Test.QuickCheck
+import Control.Applicative
 
 data Point = Point { x :: Float
                    , y :: Float
                    } deriving (Show, Eq)
+
+instance Arbitrary Point where
+  arbitrary = Point <$> arbitrary <*> arbitrary
 
 data Direction = TurnLeft | TurnRight | Straight deriving (Show, Eq)
 
@@ -16,20 +21,30 @@ getDirection a b c
           -
           ((y b) - (y a)) * ((x c) - (x a))
 
-foldDirection :: [Point] -> [Direction]
-foldDirection (x:y:z:xs) = getDirection x y z : foldDirection (y:z:xs)
-foldDirection _ = []
+directions :: [Point] -> [Direction]
+directions (x:y:z:xs) = getDirection x y z : directions (y:z:xs)
+directions _ = []
 
 convexHull :: [Point] -> [Point]
-convexHull xs = convexHull' (lowest:(sortByAngle lowest $ filter filterLowest xs))
-  where
-    lowest = lowestPoint xs
-    convexHull' (x:y:z:xs)
-    -- FIXME this is WRONG
-      | getDirection x y z == TurnLeft = x:convexHull' (y:z:xs)
-      | otherwise                      = convexHull' (x:z:xs)
-    convexHull' _ = []
-    filterLowest = (\x -> x /= lowest)
+convexHull xs
+  -- nothing to do if there's 3 or less points
+  | length xs <= 3 = xs
+  | otherwise =
+    convexHull' [] (extend (sortByAngle lowest xs))
+    where
+      lowest = lowestPoint xs
+
+      -- add first two points at the end to make it circular
+      extend xs = xs ++ [xs !! 0] ++ [xs !! 1]
+
+      convexHull' cHull (x:y:z:xs)
+        -- turn left, keep x
+        | getDirection x y z == TurnLeft = convexHull' (x:cHull) (y:z:xs)
+        -- turn right, drop y and backtrack
+        | otherwise                      = convexHull' (tail cHull) ((head cHull):x:z:xs)
+
+      -- end of the list
+      convexHull' cHull _ = reverse cHull -- reverse to make al turns ccw
 
 lowestPoint :: [Point] -> Point
 lowestPoint = minimumBy yOrdering
@@ -48,8 +63,9 @@ lowestPoint = minimumBy yOrdering
                                  then GT
                                  else EQ
 
+-- sort by polar angle and put lowest point at the beginning
 sortByAngle :: Point -> [Point] -> [Point]
-sortByAngle lowest xs = sortBy (polarAngleOrdering lowest) xs
+sortByAngle lowest xs = lowest:sortBy (polarAngleOrdering lowest) (delete lowest xs)
 
 polarAngleOrdering :: Point -> Point -> Point -> Ordering
 polarAngleOrdering lowest a b
@@ -58,9 +74,25 @@ polarAngleOrdering lowest a b
   | otherwise                       = EQ
   where
     angle p1 p2
-      | angle' < 0 = angle' + 180
+      | angle' < 0 = angle' + pi
       | otherwise  = angle'
         where
           angle' = atan (((y p2) - (y p1))/((x p2) - (x p1)))
 
-a = [Point 1.2 3.2, Point (-10) (-11), Point 2.4 17.2, Point 9.1 2.0, Point (-1.2) 2, Point (-12) 12, Point (-1.2) (-8)]
+isConvex :: [Point] -> Bool
+isConvex xs
+  | length xs <= 3 = True
+  | otherwise      = all (== TurnLeft) (directions xs)
+
+a = [Point 1.2 3.2,
+     Point (-10) (-11),
+     Point (-4) 8,
+     Point (-8) 10,
+     Point 2.4 17.2,
+     Point 9.1 2.0,
+     Point (-1.2) 2,
+     Point (-12) 12,
+     Point (-1.2) (-8)]
+
+prop_convexHull xs = isConvex $ convexHull xs
+  where types = xs :: [Point]
