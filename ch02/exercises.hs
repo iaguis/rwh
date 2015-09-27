97f0c446e570fbdda73775fd5af734caf8222a64
Iago López Galeiras <iaguis@gmail.com>
2015-09-27 04:03:16 +0200

    ch02: 2 more exercises

--- ch02/exercises.hs
+++ ch02/exercises.hs
@@ -17,3 +17,17 @@ isPalindrome xs = xs == (reverse xs)
 sortLength :: [[a]] -> [[a]]
 sortLength = sortBy comp
     where comp xs ys = compare (length' xs) (length' ys)
+
+intersperse' :: a -> [[a]] -> [a]
+intersperse' _ [] = []
+intersperse' _ [x] = x
+intersperse' sep (x:xs) = x ++ [sep] ++ intersperse' sep xs
+
+data Tree a = Node a (Tree a) (Tree a)
+    | Empty
+      deriving (Show)
+
+treeHeight :: Tree a -> Int
+treeHeight Empty = 0
+treeHeight (Node _ lt rt) = 1 + max (treeHeight lt) (treeHeight rt)
+
